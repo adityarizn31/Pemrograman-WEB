02bba6f1bbfe641f6d1e231688cb2a568e48e4fa
Aditya Rizkiawan <@adityarizkiawann@gmail.com>
2023-11-30 23:40:46 +0700

    Last Submission

---
+++ app/Views/layout/index.php
@@ -0,0 +1,216 @@
+<!-- Main -->
+<main>
+
+  <!-- Container -->
+  <div class="container">
+    <!-- Row -->
+    <div class="row">
+
+      <!-- Aside -->
+      <aside>
+        <article>
+          <header>
+            <img src="img/GNR.jpg" class="gambar_aside" alt="Foto Guns N Roses">
+          </header>
+        </article>
+      </aside>
+      <!-- End Aside -->
+
+      <!-- Flexbox -->
+      <div class="flex">
+
+        <div class="box">
+          <img src="img/GNR1.JPG" class="foto_box" alt="Berita 1">
+          <h5 class="jud_box">Guns N Roses Tour - Mexico</h5>
+          <p class="ket_box">Gunners, To Headline Power Trip In Toluco, Mexica.</p>
+          <p class="ket_box">A massive wall of sound will emerge in the desert this fall at the world’s greatest live music destination Empire Polo Club in Indio, CA with the arrival of POWER TRIP.
+            It s a one-day historic event set for November 5 that will bring together six iconic bands that define loud.</p>
+          <button class="btn_box">Go Show</button>
+        </div>
+
+
+        <div class="box">
+          <img src="img/GNR2.JPG" class="foto_box" alt="Berita 2">
+          <h5 class="jud_box">Guns N Roses Tour - Vancouver</h5>
+          <p class="ket_box">Gunners, To Headline Power Trip In Vancouver, BC.</p>
+          <p class="ket_box">The powerhouse rock band launch this jaunt on June 5 in Tel Aviv, Israel at Park Hayarkon and will continue across Europe through July 22, wrapping in Athens, Greece.
+            The band will continue onto North America, starting on Saturday, August 5 in Moncton, NB at Medavie.</p>
+          <button class="btn_box">Go Show</button>
+        </div>
+
+
+        <div class="box">
+          <img src="img/GNR3.JPG" class="foto_box" alt="Berita 3">
+          <h5 class="jud_box">Guns N Roses Tour - Sacramento</h5>
+          <p class="ket_box">Gunners, To Headline Power Trip in Sacramento</p>
+          <p class="ket_box">The West Coast’s Biggest Rock Festival, Aftershock, grows even more in 2023 with its most packed lineup yet offering 95-plus bands over four full days, led by headliners Guns N’ Roses, Tool, Avenged Sevenfold and Korn. The destination event, held at Discovery Park in Sacramento, CA on October 5-8.</p>
+          <button class="btn_box">Go Show</button>
+        </div>
+
+      </div>
+      <!-- End Flexbox -->
+
+      <div class="container">
+        <div class="col">
+
+          <div class="text-center">
+            <header>
+              <h2 class="">Album</h2>
+            </header>
+
+            <table>
+              <tr>
+                <th>Use Your Illusion II Deluxe Edition</th>
+                <th>Greatest Hits CD</th>
+              </tr>
+              <tr>
+                <td>Civil Wars</td>
+                <td>Disc 1</td>
+              </tr>
+              <tr>
+                <td>Knockin Heavens Door</td>
+                <td>Disc 2</td>
+              </tr>
+              <tr>
+                <td>Estranged</td>
+                <td>Disc 3</td>
+              </tr>
+              <tr>
+                <td>Don't Cry</td>
+                <td>Disc 4</td>
+              </tr>
+            </table>
+
+            <footer>
+              <p><small>November, 11 2022</small></p>
+            </footer>
+          </div>
+
+          <div class="text-center">
+            <header></header>
+
+            <table>
+              <tr>
+                <th>Use Your Illusion I</th>
+                <th>Greatest Hits CD</th>
+              </tr>
+              <tr>
+                <td>Live and Let Die</td>
+                <td>Disc 1</td>
+              </tr>
+              <tr>
+                <td>Perfect Crime</td>
+                <td>Disc 2</td>
+              </tr>
+              <tr>
+                <td>November Rain</td>
+                <td>Disc 3</td>
+              </tr>
+              <tr>
+                <td>Dead Horse</td>
+                <td>Disc 4</td>
+              </tr>
+            </table>
+
+            <footer>
+              <p><small>September, 17 1991</small></p>
+            </footer>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <hr>
+
+      <!-- Article 1 -->
+      <article>
+        <header>
+          <h3 class="text-left">Axl Rose</h3>
+        </header>
+        <section>
+
+          <img src="img/Axl.JPG" class="foto_article1" alt="">
+          <p class="ket_article1">This night in San Diego was truly legendary! Thank you, Gunners for the energy.</p>
+          <p class="ket_article1">Guns N' Roses is one of the legendary hard rock bands of the 80s that still exists today. Behind the success of the band which was formed in 1985, of course, cannot be separated from the figure of the eccentric vocalist, Axl Rose. Apart from vocals, his appearance always steals attention.</p>
+          <p class="ket_article1">Entering 2006, not much of Axl's style has changed. She still maintains her small braided hairstyle, ring and earrings. Not to forget, Axl also wore a brown jacket full of patterns and sunglasses.</p>
+
+        </section>
+        <footer>
+          <p><small>Last Updated 5 min ago</small></p>
+        </footer>
+      </article>
+      <!-- End Article 1 -->
+
+      <!-- Article 2 -->
+      <article>
+        <header>
+          <h3 class="text-right">Slash Crishty</h3>
+        </header>
+        <section>
+
+          <img src="img/Slash.JPG" class="foto_article2" alt="">
+          <p class="ket_article2">This night in San Diego was truly legendary! Thank you, Gunners for the energy.</p>
+          <p class="ket_article2">Guns N' Roses is one of the legendary hard rock bands of the 80s that still exists today. Behind the success of the band which was formed in 1985, of course, cannot be separated from the figure of the eccentric vocalist, Axl Rose. Apart from vocals, his appearance always steals attention.</p>
+          <p class="ket_article2">Entering 2006, not much of Axl's style has changed. She still maintains her small braided hairstyle, ring and earrings. Not to forget, Axl also wore a brown jacket full of patterns and sunglasses.</p>
+
+        </section>
+        <footer>
+          <p class="text-right"><small>Last Updated 5 min ago</small></p>
+        </footer>
+      </article>
+      <!-- End Article 2 -->
+
+      <!-- Article 3 -->
+      <article>
+        <header>
+          <h3 class="text-left">Duff McKagan</h3>
+        </header>
+        <section>
+
+          <img src="img/Duff.JPG" class="foto_article1" alt="">
+          <p class="ket_article3">This night in San Diego was truly legendary! Thank you, Gunners for the energy.</p>
+          <p class="ket_article3">Guns N' Roses is one of the legendary hard rock bands of the 80s that still exists today. Behind the success of the band which was formed in 1985, of course, cannot be separated from the figure of the eccentric vocalist, Axl Rose. Apart from vocals, his appearance always steals attention.</p>
+          <p class="ket_article3">Entering 2006, not much of Axl's style has changed. She still maintains her small braided hairstyle, ring and earrings. Not to forget, Axl also wore a brown jacket full of patterns and sunglasses.</p>
+
+        </section>
+        <footer>
+          <small>Last Updated 5 min ago</small>
+        </footer>
+      </article>
+      <!-- End Article 3 -->
+
+      <!-- Article 4 -->
+      <article>
+        <header>
+          <h3 class="text-right">Dj Ashba</h3>
+        </header>
+        <section>
+
+          <img src="img/Dj.JPG" class="foto_article2" alt="">
+          <p class="ket_article2">This night in San Diego was truly legendary! Thank you, Gunners for the energy.</p>
+          <p class="ket_article2">Daren Jay Ashba (born November 10, 1972) is an American musician, guitarist, songwriter, record producer, and graphic designer. He was the lead guitarist of Sixx:A.M. He is also known for his work with hard rock bands BulletBoys, Beautiful Creatures and Guns N' Roses.</p>
+          <p class="ket_article2">Entering 2006, not much of Axl's style has changed. She still maintains her small braided hairstyle, ring and earrings. Not to forget, Axl also wore a brown jacket full of patterns and sunglasses.</p>
+
+        </section>
+        <footer>
+          <p class="text-right"><small>Last Updated 5 min ago</small></p>
+        </footer>
+      </article>
+      <!-- End Article 4 -->
+
+    </div>
+    <!-- End Row -->
+  </div>
+  <!-- End Container -->
+
+</main>
+<!-- End Main -->
+
+<!-- Footer  -->
+<footer class="footer">
+  Copyright © 2023 Guns N' Roses
+</footer>
+<!-- End Footer -->
---
+++ app/Views/templates/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light text-center">
+  <div class="container">
+    <a class="navbar-brand" href="/pages/index">GUNS N ROSES</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.gunsnroses.com/media">MEDIA</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://gnrmerch.com/collections/music">MUSIC</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
